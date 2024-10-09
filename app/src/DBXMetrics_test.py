68497006c5d80c03921a444be70fe8e8d71aee91
lrtbrabo <lrtbrabo>
2024-10-09 21:33:31 +0000

    Adding things to keep the execution running in the databricks runtime

--- app/src/DBXMetrics_test.py
+++ app/src/DBXMetrics_test.py
@@ -1,7 +1,13 @@
 # Databricks notebook source
+# MAGIC %pip install --quiet databricks-sdk==0.9.0
+# MAGIC dbutils.library.restartPython()
+
+# COMMAND ----------
 
 from DBXMetrics import DBXMetrics
 from _spark_runtime import get_spark
+
+# COMMAND ----------
 
 def run_my_workload():
     spark = get_spark()
@@ -12,5 +18,20 @@ def run_my_workload():
     stagemetrics.end()
 
     stagemetrics.persist() 
+    return stagemetrics._stage_metrics(), stagemetrics._aggregate_metrics()
 
-run_my_workload()
+# COMMAND ----------
+
+_stage_metrics, _aggregate_metrics = run_my_workload()
+
+# COMMAND ----------
+
+_stage_metrics.display()
+
+# COMMAND ----------
+
+_aggregate_metrics.display()
+
+# COMMAND ----------
+
+
