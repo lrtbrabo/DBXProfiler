cec892b93cee1ecde3751cc87f412c1e14e851fd
lrtbrabo <lrtbrabo>
2024-10-09 21:42:19 +0000

    fix: Forgot to change the function signature in the test call

--- app/src/DBXMetrics_test.py
+++ app/src/DBXMetrics_test.py
@@ -11,7 +11,7 @@ from _spark_runtime import get_spark
 
 def run_my_workload():
     spark = get_spark()
-    stagemetrics = DBXMetrics("lucas_test", "test_catalog")
+    stagemetrics = DBXMetrics("lucas_test")
 
     stagemetrics.begin()
     spark.sql("select count(*) from range(1000) cross join range(1000) cross join range(1000)").show()
