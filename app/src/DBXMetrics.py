9bdffb43eb77c20fefdbbeff2d45a5c0442a64d9
Lucas Brabo <lucas.brabo@databricks.com>
2024-10-09 17:18:09 -0300

    Adding main abstraction for SparkMeasure agent to use in the notebooks

---
+++ app/src/DBXMetrics.py
@@ -0,0 +1,59 @@
+from sparkmeasure import StageMetrics
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import current_timestamp, current_date, lit
+from _spark_runtime import get_workspace, get_spark
+from dataGateway import DataGateway
+import uuid
+
+class DBXMetrics(StageMetrics):
+    def __init__(self, application_name: str, catalog: str):
+        super().__init__(get_spark())
+        self.w = get_workspace()
+        self.application_name = application_name
+        self.execution_id = str(uuid.uuid4())
+        self.catalog = catalog
+
+    def start_metrics(self):
+        self.begin()
+
+    def stop_metrics(self):
+        self.end()
+
+    def _add_metadata(self, dataframe: DataFrame) -> DataFrame:
+        """
+        Using the method withColumn instead of with Columns
+        for backwards compatibility with previous spark versions
+        """
+        notebook_path = self.w.dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get()
+        notebook_name = notebook_path.split('/')[-1] 
+        dataframe = dataframe.withColumn("__execution_timestamp", current_timestamp())
+        dataframe = dataframe.withColumn("__execution_date", current_date())
+        dataframe = dataframe.withColumn("__application_entrypoint_path", lit(notebook_path))
+        dataframe = dataframe.withColumn("__application_entrypoint_file", lit(notebook_name)) 
+        dataframe = dataframe.withColumn("__application_name", lit(self.application_name))
+        dataframe = dataframe.withColumn("__execution_id", lit(self.execution_id))
+        return dataframe
+
+    def _stage_metrics(self):
+        df_stage_metrics = self.create_stagemetrics_DF("PerfStageMetrics")
+        df_stage_metrics = self._add_metadata(df_stage_metrics)
+        return df_stage_metrics
+
+    def _aggregate_metrics(self):
+        df_aggregated_metrics = self.aggregate_stagemetrics_DF("PerfStageMetrics")
+        df_aggregated_metrics = self._add_metadata(df_aggregated_metrics)
+        return df_aggregated_metrics
+
+    def persist(self) -> DataGateway:
+        _stage_metrics = self._stage_metrics()
+        _aggregate_metrics = self._aggregate_metrics()
+        options = {"catalog": self.catalog}
+        gtw = (
+            DataGateway(
+                stage_metrics=_stage_metrics,
+                agg_metrics=_aggregate_metrics
+            )
+                .option("unity_catalog", **options)
+        )
+        return gtw
+
---
+++ app/src/dataGateway.py
@@ -0,0 +1,34 @@
+from pyspark.sql import DataFrame
+
+"""
+This part of the code is critical, 
+this cannot cause a fail in the running jobs no errors should be thrown here
+"""
+
+class DataGateway:
+    def __init__(self, stage_metrics: DataFrame, agg_metrics: DataFrame):
+        self.stage_metrics = stage_metrics
+        self.agg_metrics = agg_metrics
+
+    def option(self, entry: str, **kwargs):
+        method = getattr(self, entry, None)
+        if callable(method):
+            return method(**kwargs)
+        else:
+            raise AttributeError(f"Method {entry} not found")
+
+    def unity_catalog(self, **kwargs):
+        UnityCatalog(**kwargs)
+
+    def temp(self, **kwargs):
+        TempSave(**kwargs)
+
+
+class UnityCatalog:
+    def __init__(self):
+        print("I'm saving this to unity!!!")
+
+
+class TempSave:
+    def __init__(self):
+        print("I'm saving this to temp!!!")
