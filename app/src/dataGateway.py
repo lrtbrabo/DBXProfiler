748c4ac92c481255b0107f6a9a702d9161de485a
Lucas Brabo <lucas.brabo@databricks.com>
2024-10-10 15:54:25 -0300

    changing Data Gateway savers to implement abstract method

--- app/src/dataGateway.py
+++ app/src/dataGateway.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from pyspark.sql import DataFrame
 
 """
@@ -5,10 +6,30 @@ This part of the code is critical,
 this cannot cause a fail in the running jobs no errors should be thrown here
 """
 
+class DataSaver(ABC):
+    @abstractmethod
+    def save(self, stage_metrics: DataFrame, agg_metrics: DataFrame, **kwargs):
+        pass
+
+class UnityCatalog(DataSaver):
+    def save(self, stage_metrics: DataFrame, agg_metrics: DataFrame, **kwargs):
+        catalog = kwargs.get("catalog")
+        print(f"Saving to Unity Catalog: {catalog}")
+
+class TempSave:
+    def save(self, stage_metrics: DataFrame, agg_metrics: DataFrame, **kwargs):
+        path = kwargs.get('path')
+        print(f"Saving to temprary location: {path}")
+        
+
 class DataGateway:
     def __init__(self, stage_metrics: DataFrame, agg_metrics: DataFrame):
         self.stage_metrics = stage_metrics
         self.agg_metrics = agg_metrics
+        self.savers = {
+            'unity_catalog': UnityCatalog(),
+            'temp': TempSave()
+        }
 
     def option(self, entry: str, **kwargs):
         """
@@ -18,24 +39,8 @@ class DataGateway:
             - temp
                 - path
         """
-        method = getattr(self, entry, None)
-        if callable(method):
-            return method(**kwargs)
+        saver = self.savers.get(entry)
+        if saver:
+            return saver.save(self.stage_metrics, self.agg_metrics, **kwargs)
         else:
             raise AttributeError(f"Method {entry} not found")
-
-    def unity_catalog(self, **kwargs):
-        UnityCatalog(**kwargs)
-
-    def temp(self, **kwargs):
-        TempSave(**kwargs)
-
-
-class UnityCatalog:
-    def __init__(self, **kargs):
-        print("I'm saving this to unity!!!")
-
-
-class TempSave:
-    def __init__(self, **kargs):
-        print("I'm saving this to temp!!!")
