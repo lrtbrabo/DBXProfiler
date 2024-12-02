cb30eb55bde9c50ca9cd8c8eac3c168e6a54ec8e
Lucas Brabo <lucas.brabo@databricks.com>
2024-12-02 18:35:04 -0300

    adding new version to allowed databricks runtimes

--- src/dbxmetrics/config.py
+++ src/dbxmetrics/config.py
@@ -64,6 +64,10 @@ def _get_allowed_databricks_runtimes():
         "13.3.x-scala2.12": {
             "maven_coordinate": "ch.cern.sparkmeasure:spark-measure_2.12:0.24",
             "pypi_package": "sparkmeasure==0.24.0"
+        },
+        "14.3.x-scala2.12": {
+            "maven_coordinate": "ch.cern.sparkmeasure:spark-measure_2.12:0.24",
+            "pypi_package": "sparkmeasure==0.24.0"
         }
     }
 
