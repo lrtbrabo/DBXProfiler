454ac6ca65296c9f2ca39e9312d377f551e70190
Lucas Brabo <lucas.brabo@databricks.com>
2024-12-03 15:30:55 -0300

    adding compatibility to 12.2.x-scala2.12

--- src/dbxmetrics/config.py
+++ src/dbxmetrics/config.py
@@ -61,6 +61,10 @@ def get_workspace() -> WorkspaceClient:
 
 def _get_allowed_databricks_runtimes():
     return {
+        "12.2.x-scala2.12": {
+            "maven_coordinate": "ch.cern.sparkmeasure:spark-measure_2.12:0.24",
+            "pypi_package": "sparkmeasure==0.24.0"
+        },
         "13.3.x-scala2.12": {
             "maven_coordinate": "ch.cern.sparkmeasure:spark-measure_2.12:0.24",
             "pypi_package": "sparkmeasure==0.24.0"
